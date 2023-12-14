de593e6d427a33c17562c65ee5ccf5ee6b2b3d66
michaelgzy <429534533@qq.com>
2023-12-14 14:11:24 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Goyyds',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'It\'s cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
