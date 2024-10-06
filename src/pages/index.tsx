da41e49feaf511461f2e76ec96c62f99f41766d3
michael <429534533@qq.com>
2024-10-06 10:10:14 +0800

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,13 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog">
-              Let's start the journey !⏱️
-          </Link>
-        </div>
+        {/*<div className={styles.buttons}>*/}
+        {/*  <Link*/}
+        {/*    className="button button--secondary button--lg"*/}
+        {/*    to="/blog">*/}
+        {/*      Let's start the journey !⏱️*/}
+        {/*  </Link>*/}
+        {/*</div>*/}
       </div>
     </header>
   );
